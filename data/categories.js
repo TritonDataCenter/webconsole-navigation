69f9cf29c0355b37e2d16987981700dd239bca60
Sérgio Ramos <mail@sergioramos.me>
2018-03-23 19:01:46 +0000

    update dependencies

--- data/categories.js
+++ data/categories.js
@@ -1,27 +1,88 @@
 module.exports = [
   {
     name: 'Compute',
-    slug: 'compute',
     services: [
       {
         name: 'VMs & Containers',
-        slug: 'vms_containers',
-        description: 'Run VMs and bare metal containers',
-        url: '/instaces',
-        tags: []
+        slug: 'instances',
+        description: 'Run VMs and bare metal containers'
+      }
+    ]
+  },
+  {
+    name: 'Network',
+    services: [
+      {
+        name: 'VLANs',
+        slug: 'vlans',
+        description: 'Wire your application your way'
+      },
+      {
+        name: 'Subnets',
+        slug: 'subnets',
+        description: 'A network for everything'
+      },
+      {
+        name: 'Firewall Rules',
+        slug: 'firewall',
+        description: 'Control the bits coming and going'
+      }
+    ]
+  },
+  {
+    name: 'Storage',
+    services: [
+      {
+        name: 'Triton Object Storage',
+        slug: 'object-storage',
+        description: 'Modern cloud object storage',
+        tags: ["'note'='was Manta'"]
+      },
+      {
+        name: 'S3 Compatibility Bridge',
+        slug: 's3-bridge',
+        description: 'Modern storage, legacy compability'
+      },
+      {
+        name: 'Triton Volumes',
+        slug: 'volumes',
+        description: 'Network filesystems for your apps',
+        tags: ["'is-new'='true'"]
+      }
+    ]
+  },
+  {
+    name: 'Access',
+    services: [
+      {
+        name: 'Role Based Access Control',
+        slug: 'rbac',
+        description: 'Manage users within your account'
       }
     ]
   },
   {
     name: 'Help & Support',
-    slug: 'help_support',
     services: [
       {
         name: 'Service Status',
-        slug: 'service_status',
-        description: 'Find out about the status of our services',
-        url: '/status',
-        tags: []
+        slug: 'status',
+        description: 'Find out about the status of our services'
+      },
+      {
+        name: 'Contact Support',
+        slug: 'contact-support',
+        description: 'Chat to us via phone or email'
+      },
+      {
+        name: 'Support Plans',
+        slug: 'support-plans',
+        description: 'Write here about Support Plans'
+      },
+      {
+        name: 'Getting Started',
+        slug: 'getting-started',
+        description: 'Write here about Getting Started'
       }
     ]
   }
